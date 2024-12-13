cd93e1a0ddd4b73850eac78b8928e3d7609852a6
Mustafa Ahmed <contactmustafa8@gmail.com>
2024-12-13 12:46:26 +0500

    fix Outh

--- src/lib/oauth.ts
+++ src/lib/oauth.ts
@@ -22,12 +22,10 @@ export async function signUpWithGithub() {
 export async function signUpWithGoogle() {
   const { account } = await CreateAdminClient();
 
-  const origin = headers().get("origin");
-
   const redirectUrl = await account.createOAuth2Token(
     OAuthProvider.Google,
-    `${origin}/oauth`,
-    `${origin}/sign-up`
+    `${process.env.NEXT_PUBLIC_APP_URL}/oauth`,
+    `${process.env.NEXT_PUBLIC_APP_URL}/sign-up`
   );
 
   return redirect(redirectUrl);
