df6d51abc829300bf6e3fbf2308384b3691d8ee1
Mustafa Ahmed <contactmustafa8@gmail.com>
2024-12-13 12:10:10 +0500

    Zcrum

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -8,10 +8,11 @@ import tasks from "@/features/tasks/server/route";
 
 const app = new Hono().basePath("/api");
 
-app
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const routes = app
   .route("/auth", auth)
   .route("/workspaces", workspaces)
-  .route("/members", members)
+  .route("members", members)
   .route("/projects", projects)
   .route("/tasks", tasks);
 
@@ -20,4 +21,4 @@ export const POST = handle(app);
 export const PATCH = handle(app);
 export const DELETE = handle(app);
 
-export type AppType = typeof app; // Use `app` instead of `routes`
+export type AppType = typeof routes;
