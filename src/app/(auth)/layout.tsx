02255a8606b9e00a7d366fe090654604ff49f7b6
Mustafa Ahmed <mustafaahmedsecuremail@gmail.com>
2025-08-15 12:36:38 +0500

    Auth layout Set

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
 import Link from "next/link";
+import Image from "next/image";
 import { usePathname } from "next/navigation";
+import { Button } from "@/components/ui/button";
 
 interface AuthlayoutProps {
   children: React.ReactNode;
