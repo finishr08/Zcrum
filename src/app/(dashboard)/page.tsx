a693040e31881bd3c3c9032efc6ee41c66d1e6ec
Mustafa Ahmed <mustafaahmedsecuremail@gmail.com>
2025-08-15 12:43:54 +0500

    Dashboard Page Set

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,6 +1,6 @@
+import { redirect } from "next/navigation";
 import { getCurrent } from "@/features/auth/queries";
 import { getWorkspaces } from "@/features/workspaces/queries";
-import { redirect } from "next/navigation";
 
 export default async function Home() {
   const user = await getCurrent();
