4cfae37f102b3fd152e18201caee0c3778d159fd
Mustafa Ahmed <mustafaahmedsecuremail@gmail.com>
2025-08-15 13:18:11 +0500

    Workspace Api Update

--- src/features/workspaces/api/use-create-workspace.ts
+++ src/features/workspaces/api/use-create-workspace.ts
@@ -1,8 +1,8 @@
-import { client } from "@/lib/rpc";
 import { toast } from "sonner";
+import { client } from "@/lib/rpc";
+import { useRouter } from "next/navigation";
 import { InferRequestType, InferResponseType } from "hono";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { useRouter } from "next/navigation";
 
 type ResponseType = InferResponseType<(typeof client.api.workspaces)["$post"]>;
 type RequestType = InferRequestType<(typeof client.api.workspaces)["$post"]>;
