c6bd0f9163e61f5bcaa6a3df1835c4ca4ab000c1
Mustafa Ahmed <mustafaahmedsecuremail@gmail.com>
2025-08-15 12:59:11 +0500

    useLogout Api Update

--- src/features/auth/api/use-logout.ts
+++ src/features/auth/api/use-logout.ts
@@ -1,8 +1,8 @@
-import { client } from "@/lib/rpc";
 import { toast } from "sonner";
+import { client } from "@/lib/rpc";
 import { InferResponseType } from "hono";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 type ResponseType = InferResponseType<(typeof client.api.auth.logout)["$post"]>;
 
