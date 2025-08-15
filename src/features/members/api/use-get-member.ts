1dc429a09ae2c180c8509d8c25b2dbefea4ca67f
Mustafa Ahmed <mustafaahmedsecuremail@gmail.com>
2025-08-15 13:03:41 +0500

    Members APIS Update

--- src/features/members/api/use-get-member.ts
+++ src/features/members/api/use-get-member.ts
@@ -1,5 +1,5 @@
-import { useQuery } from "@tanstack/react-query";
 import { client } from "@/lib/rpc";
+import { useQuery } from "@tanstack/react-query";
 
 interface useGetMembersProps {
   workspaceId: string;
