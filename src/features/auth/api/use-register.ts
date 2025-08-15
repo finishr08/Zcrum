7c68d4fc22be057372a0aca0257600603ab45130
Mustafa Ahmed <mustafaahmedsecuremail@gmail.com>
2025-08-15 12:59:38 +0500

    useregister Api Update

--- src/features/auth/api/use-register.ts
+++ src/features/auth/api/use-register.ts
@@ -1,8 +1,8 @@
-import { client } from "@/lib/rpc";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { InferRequestType, InferResponseType } from "hono";
-import { useRouter } from "next/navigation";
 import { toast } from "sonner";
+import { client } from "@/lib/rpc";
+import { useRouter } from "next/navigation";
+import { InferRequestType, InferResponseType } from "hono";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 type ResponseType = InferResponseType<
   (typeof client.api.auth.register)["$post"]
